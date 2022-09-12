fc81a79dae36c678cc01fe93815b20de0ef3fb42
IDG9I <damiangamerxd123@gmail.com>
2022-09-12 16:28:13 -0300

    version 1

---
+++ app/src/main/java/com/csdexample/newcalculadora/MainActivity.java
@@ -0,0 +1,19 @@
+package com.csdexample.newcalculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void secundary (View view){
+        Intent Secundary = new Intent();
+    }
+}
---
+++ app/src/main/java/com/csdexample/newcalculadora/secundary.java
@@ -0,0 +1,13 @@
+package com.csdexample.newcalculadora;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class secundary extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secundary);
+    }
+}
